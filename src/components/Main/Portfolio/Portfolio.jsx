9e6fb15f2e2cdb64b5047257d40c56e310c1d2ef
yryryk <tovchennikov@e1.ru>
2023-04-18 17:47:53 +0500

    feat: добавить секцию Portfolio

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -3,6 +3,21 @@ import './Portfolio.css';
 function Portfolio() {
   return (
     <section className="portfolio">
+      <h2 className="portfoljo__title">Портфолио</h2>
+      <ul className="portfoljo__list">
+        {[
+          {link: "https://github.com/yryryk/how-to-learn", name: "Статичный сайт"},
+          {link: "https://github.com/yryryk/russian-travel", name: "Адаптивный сайт"},
+          {link: "https://github.com/yryryk/react-mesto-api-full-gha", name: "Одностраничное приложение"}
+        ].map((item, i) => (
+          <li key={i} className="portfoljo__list-item">
+            <a href={item.link} target="_blank" rel="noreferrer" className="portfoljo__list-link">
+              {item.name}
+              <div className="portfoljo__list-arrow"></div>
+            </a>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
--- src/components/Main/Techs/Techs.jsx
+++ src/components/Main/Techs/Techs.jsx
@@ -8,13 +8,7 @@ function Techs() {
       <h3 className="techs__about-title">7 технологий</h3>
       <p className="techs__about-paragraph">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
       <ul className="techs__list">
-        <li className="techs__tech">HTML</li>
-        <li className="techs__tech">CSS</li>
-        <li className="techs__tech">JS</li>
-        <li className="techs__tech">React</li>
-        <li className="techs__tech">Git</li>
-        <li className="techs__tech">Express.js</li>
-        <li className="techs__tech">mongoDB</li>
+        {["HTML", "CSS", "JS", "React", "Git", "Express.js", "mongoDB"].map((tech, i) => (<li key={i} className="techs__tech">{tech}</li>))}
       </ul>
     </section>
   );
