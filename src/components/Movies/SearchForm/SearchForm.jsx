3607dd72240b2933b7cb4f94b2662ad589963317
yryryk <tovchennikov@e1.ru>
2023-05-07 19:24:16 +0500

    fix: исправить после ревью

--- src/components/Movies/SearchForm/SearchForm.jsx
+++ src/components/Movies/SearchForm/SearchForm.jsx
@@ -5,10 +5,10 @@ function SearchForm(props) {
   const {handleSearch} = props;
   return (
     <section className="search-form">
-      <label className="search-form__label">
-        <input type="text" className="search-form__search-input" placeholder="Фильм" search="true" />
-        <button aria-label="Поиск" type="button" className="search-form__search-button button" onClick={handleSearch}></button>
-      </label>
+      <form className="search-form__label">
+        <input type="text" className="search-form__search-input" placeholder="Фильм" search="true" required />
+        <button aria-label="Поиск" type="submit" className="search-form__search-button button" onClick={handleSearch}></button>
+      </form>
       <Switcher className="search-form__switcher"><p className="search-form__switcher-name">Короткометражки</p></Switcher>
     </section>
   );
